5839a6c34f34236100aaccddb0ef686e121c0152
Mingqiang Sun <davidmusksun1993@gmail.com>
2021-09-21 15:05:44 +0800

    remove __auto_type declaration in macro
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- st.h
+++ st.h
@@ -4,10 +4,10 @@ typedef int(*compare)(const void*,const void*);
 
 #define SWAP_POINTER(x,y) \
 do{\
-    __auto_type __x=&(x);/*avoid side effect*/\
-    __auto_type __y=&(y);\
+    void**__x=&(x);/*avoid side effect*/\
+    void**__y=&(y);\
     if(__x==__y)break;\
-    __auto_type __t=*__x;\
+    void*__t=*__x;\
     *__x=*__y;\
     *__y=__t;\
 }while(0)
